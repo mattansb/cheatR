992492dd9e354bc2d58f6cd514039719d542a6cd
mattansb <35330040+mattansb@users.noreply.github.com>
2018-11-16 16:40:57 +0200

    new plotting (graph and hist) functions

--- R/methods.R
+++ R/methods.R
@@ -3,12 +3,11 @@
 #' @author Mattan S. Ben-Shachar
 #' @param x output of \code{catch_em()}.
 #' @param ... not used
+#' @importFrom scales percent
 #' @export
 print.chtrs <- function(x,...) {
-  if ("scales" %in% rownames(installed.packages())) {
-    x$results[] <- scales::percent(x$results[])
-    x$results[upper.tri(x$results)] <- ''
-  }
+  x$results[] <- percent(x$results[])
+  x$results[upper.tri(x$results)] <- ''
 
   print(x$results, quote = F, right = T)
 
@@ -43,4 +42,45 @@ summary.chtrs <- function(object, bad_files = F) {
   } else {
     object$results
   }
+}
+
+#' plot cheatrs
+#'
+#' @author Mattan S. Ben-Shachar
+#' @param object output of \code{catch_em()}.
+#' @param weight_range range of edge values to plot
+#' @param ... not used
+#'
+#' @import tidygraph
+#' @import ggraph
+#' @import ggplot2
+#' @importFrom scales percent
+#' @importFrom magrittr %>%
+#' @export
+graph_em <- function(object,weight_range = c(.7,1),...){
+  results_graph <- summary(object) %>%
+    as_tbl_graph() %>%
+    activate(what = edges) %>%
+    filter(!is.na(weight),
+           weight > weight_range[1],
+           weight < weight_range[2])
+
+  ggraph(results_graph) +
+    geom_edge_fan(aes(label = percent(weight)),
+                  angle_calc = 'along',
+                  label_dodge = unit(2.5, 'mm')) +
+    geom_node_label(aes(label = name))
+}
+
+#' plot histogram of similarity scores
+#'
+#' @author Mattan S. Ben-Shachar
+#' @param object output of \code{catch_em()}.
+#' @param ... passed to \code{hist}
+#' @export
+hist.chtrs <- function(object,...) {
+  hist(summary(object)[],
+       main = 'Histogram of similarity scores',
+       xlab = 'Similarity',
+       ...)
 }
